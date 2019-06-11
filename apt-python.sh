616a752eb172f4f33b91ff3749c0551b736339e7
zhangtianjie <715751607@qq.com>
2019-06-11 14:57:54 +0800

    update apt-python.sh

--- apt-python.sh
+++ apt-python.sh
@@ -6,10 +6,15 @@
 # Email: ztj1993@gmail.com
 # LastDate: 2019-06-11
 # Version: 1.0.0
+# Use: curl -sSL https://dwz.cn/imrR3eVN | sh
 ###############
+
+set -e
+set -x
 
 apt-get install -y wget
 
+rm -rf get-pip.py
 wget https://bootstrap.pypa.io/get-pip.py
 
 apt-get install -y libssl-dev libcurl4-openssl-dev
