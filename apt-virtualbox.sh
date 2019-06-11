7688450c79d9ac1428632c1475fee2698f55eb36
zhangtianjie <715751607@qq.com>
2019-06-11 15:06:10 +0800

    update apt-virtualbox.sh

--- apt-virtualbox.sh
+++ apt-virtualbox.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 ###############
-# Name: Debian/Ubuntu install VirtualBox
+# Name: apt install virtualbox
 # Author: Ztj
 # Email: ztj1993@gmail.com
 # LastDate: 2019-06-11
 # Version: 1.0.0
+# Use: curl -sSL https://dwz.cn/gh4NlNLr | sh
 ###############
+
+set -e
+set -x
 
 deb_source="deb https://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib"
 deb_file="/etc/apt/sources.list.d/virtualbox.list"
 echo ${deb_source} >> ${deb_file}
 
-apt-key add oracle_vbox_2016.asc
-apt-key add oracle_vbox.asc
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 
 apt-get update
-apt-get install virtualbox-6.0
+apt-get install -y virtualbox-6.0
