3dd6e774f37bf39d5193a9232d9610c9cbdd5b33
zhangtianjie <715751607@qq.com>
2019-06-19 15:00:33 +0800

    update apt-virtualbox.sh

--- apt-virtualbox.sh
+++ apt-virtualbox.sh
@@ -4,9 +4,13 @@
 # Name: apt install virtualbox
 # Author: Ztj
 # Email: ztj1993@gmail.com
-# LastDate: 2019-06-11
-# Version: 1.0.0
-# Use: curl -sSL https://dwz.cn/gh4NlNLr | sh
+# LastDate: 2019-06-19
+# Use:
+#     wget https://raw.githubusercontent.com/ztj1993/shell/master/apt-virtualbox.sh
+#     chmod +x apt-virtualbox.sh
+#     sudo ./apt-virtualbox.sh
+#     --- or ---
+#     curl -sSL https://dwz.cn/gh4NlNLr | sudo sh
 ###############
 
 set -e
@@ -14,10 +18,17 @@ set -x
 
 deb_source="deb https://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib"
 deb_file="/etc/apt/sources.list.d/virtualbox.list"
-echo ${deb_source} >> ${deb_file}
+echo ${deb_source} | tee ${deb_file}
 
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+rm -rf oracle_vbox_2016.asc
+wget http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc
+apt-key add oracle_vbox_2016.asc
+rm -rf oracle_vbox_2016.asc
+
+rm -rf oracle_vbox.asc
+wget http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc
+apt-key add oracle_vbox.asc
+rm -rf oracle_vbox.asc
 
 apt-get update
 apt-get install -y virtualbox-6.0
