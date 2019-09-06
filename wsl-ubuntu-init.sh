4ed012527cdbba4f688171e4f21bab5ff794cf81
Ztj <ztj1993@gmail.com>
2019-09-06 11:23:28 +0800

    Update and rename wsl-init.sh to wsl-ubuntu-init.sh

---
+++ wsl-ubuntu-init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+###############
+# Name: windows subsystem for linux(ubuntu) init
+# Author: Ztj
+# Email: ztj1993@gmail.com
+# LastDate: 2019-06-24
+# Use:
+#     wget https://raw.githubusercontent.com/ztj1993/shell/master/wsl-ubuntu-init.sh
+#     chmod +x wsl-ubuntu-init.sh
+#     sudo ./wsl-ubuntu-init.sh
+###############
+
+set -e
+set -x
+
+# 免密码
+echo "$(whoami) ALL=(ALL) NOPASSWD:ALL" | tee /etc/sudoers.d/$(whoami)
+
+# 基础库
+apt-get install -y libssl-dev libcurl4-openssl-dev
+
+# 基础软件
+apt-get install -y curl wget git vim
+
+# GoLang
+sudo apt-get install -y golang
+
+# C/C++
+sudo apt-get install -y build-essential
