56892043a1e9efb35ccf7916b2b952c92b8c8a55
Ztj <ztj1993@gmail.com>
2021-03-26 17:46:15 +0800

    update wsl-ubuntu-init.sh

--- wsl-ubuntu-init.sh
+++ wsl-ubuntu-init.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
 ###############
-# Name: windows subsystem for linux(ubuntu) init
+# Name: WSL Ubuntu 批处理
 # Author: Ztj
 # Email: ztj1993@gmail.com
-# LastDate: 2019-06-24
-# Use:
-#     wget https://raw.githubusercontent.com/ztj1993/shell/master/wsl-ubuntu-init.sh
-#     chmod +x wsl-ubuntu-init.sh
-#     sudo ./wsl-ubuntu-init.sh
+# CreateDate: 2019-06-24
+# UpdateDate: 2021-01-12
 ###############
 
-set -e
-set -x
-
-# 免密码
-echo "$(whoami) ALL=(ALL) NOPASSWD:ALL" | tee /etc/sudoers.d/$(whoami)
+# 用户免密码
+./setting-sudo-nopasswd.sh
 
 # 基础软件
-apt-get install -y curl wget git vim
+sudo apt-get install -y curl wget git vim
