f250a032519d7af8171f4bfc2a979322b18eedc4
Ztj <ztj1993@gmail.com>
2021-03-26 11:40:05 +0800

    update apt-aliyun-mirror.sh

--- apt-aliyun-mirror.sh
+++ apt-aliyun-mirror.sh
@@ -4,20 +4,16 @@
 # Name: apt aliyun mirror
 # Author: Ztj
 # Email: ztj1993@gmail.com
-# LastDate: 2019-06-19
-# Use:
-#     wget https://raw.githubusercontent.com/ztj1993/shell/master/apt-aliyun-mirror.sh
-#     chmod +x apt-aliyun-mirror.sh
-#     sudo ./apt-aliyun-mirror.sh
+# CreateDate: 2019-06-19
+# UpdateDate: 2021-03-26
 ###############
 
 set -e
 
-date_time=$(date +%Y-%m-%d-%H-%M-%S)
-cp /etc/apt/sources.list /etc/apt/sources.list.backup.${date_time}
+cp /etc/apt/sources.list /etc/apt/sources.list.backup.$(date +%Y-%m-%d-%H-%M-%S)
 
-search="[a-z0-9\.]*.ubuntu.com"
-replace="mirrors.aliyun.com"
-sed -i "s/${search}/${replace}/g" /etc/apt/sources.list
+search="http://[a-z0-9\.]*/ubuntu"
+replace="http://mirrors.aliyun.com/ubuntu"
+sed -i "s@${search}@${replace}@g" /etc/apt/sources.list
 
-apt-get -y update
+apt -y update
